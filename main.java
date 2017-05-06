3c962f96eb8164c9967b104893ee94870461a71d
00357863 <wannasit.chaiphinan@krungsri.com>
2017-05-06 11:30:06 +0700

    split line

--- main.java
+++ main.java
@@ -1,7 +1,10 @@
 class Test {
     public static void main(String [] args){
+        System.out.println("my name is nong");
         System.out.println("Hello world");
+        System.out.println("my name is tom");
          System.out.println("Hello world2");
+         System.out.println("my name is stop");
          System.out.println("Hello world3");
     }
 }
