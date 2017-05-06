458126a675131f4885ea8b21d103979d5066768f
00357863 <wannasit.chaiphinan@krungsri.com>
2017-05-06 11:21:20 +0700

    create sysout2

--- main.java
+++ main.java
@@ -2,5 +2,6 @@ class Test {
     public static void main(){
         System.out.println("Hello world");
          System.out.println("Hello world2");
+         System.out.println("Hello world3");
     }
 }
