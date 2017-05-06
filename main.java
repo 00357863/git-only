ce6c300b05b014cc3729c04284fbea167fd161a3
00357863 <wannasit.chaiphinan@krungsri.com>
2017-05-06 11:40:06 +0700

    line2

--- main.java
+++ main.java
@@ -2,7 +2,7 @@ class Test {
     public static void main(String [] args){
         System.out.println("my name is nong1");
         System.out.println("Hello world");
-        System.out.println("my name is tom");
+        System.out.println("my name is tom1");
          System.out.println("Hello world2");
          System.out.println("my name is stop");
          System.out.println("Hello world3");
