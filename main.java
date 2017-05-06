f2d1c9e642a02e415b80e54a6a57380f05ca4cf9
00357863 <wannasit.chaiphinan@krungsri.com>
2017-05-06 11:40:24 +0700

    line3

--- main.java
+++ main.java
@@ -4,7 +4,7 @@ class Test {
         System.out.println("Hello world");
         System.out.println("my name is tom1");
          System.out.println("Hello world2");
-         System.out.println("my name is stop");
+         System.out.println("my name is stop1");
          System.out.println("Hello world3");
     }
 }
