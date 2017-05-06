86289ac899542e61910178eef2814fbc654def72
00357863 <wannasit.chaiphinan@krungsri.com>
2017-05-06 12:04:07 +0700

    edit command  e

--- main.java
+++ main.java
@@ -3,6 +3,7 @@ class Test {
         System.out.println("my name is nong1");
         System.out.println("Hello world");
         System.out.println("my name is tom1");
+        System.out.println("my name is Camp");
          System.out.println("Hello world2");
          System.out.println("my name is stop1");
          System.out.println("Hello world3");
